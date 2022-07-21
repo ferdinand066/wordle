938f64d38f3307873bdda6b925a0d9453b8d87c1
Ferdinand Gunawan <ferdinand.gunawan@binus.edu>
2022-07-21 14:22:57 +0700

    Fix bug change color of keyboard

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,4 +1,5 @@
 import styles from './Keyboard.module.css';
+import mainStyle from '../App.module.css';
 
 export default function Keyboard({ characterLists, handleKeyInput, gameOver } : any){
     function triggerKeydown(key: string){
@@ -11,6 +12,9 @@ export default function Keyboard({ characterLists, handleKeyInput, gameOver } : 
         let result = "";
 
         classList.forEach(className => {
+            if (['true', 'wrong', 'miss'].includes(className)){
+                return result += (" " + mainStyle[className]);
+            }
             return result += (" " + styles[className]);
         })
 
@@ -21,6 +25,7 @@ export default function Keyboard({ characterLists, handleKeyInput, gameOver } : 
         <div className={ styles.keyboard }>
             {
                 Object.keys(characterLists).map((key, index) => {
+                    console.log(characterLists[key])
                     return (
                       <div className={ getClassName(characterLists[key].className) } key={ key } style={{ gridColumn: `span ${ characterLists[key].size} / span ${ characterLists[key].size}` }}  onClick={ () => triggerKeydown(key) }>{ characterLists[key].logo }</div>
                     )
